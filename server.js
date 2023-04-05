d9629858dad8a645dffa0ab7db58853890d56a7d
Vitalii Popovych <popovych@gmail.com>
2023-04-04 19:54:22 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function (req, res) {
+  const mascots = [
+    { name: 'Sammy', organization: 'DigitalOcean', birth_year: 2012 },
+    { name: 'Tux', organization: 'Linux', birth_year: 1996 },
+    { name: 'Moby Dock', organization: 'Docker', birth_year: 2013 },
+  ];
+  const tagline =
+    'No programming concept is complete without a cute animal mascot.';
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline,
+  });
+});
+
+// about page
+app.get('/about', function (req, res) {
+  res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
