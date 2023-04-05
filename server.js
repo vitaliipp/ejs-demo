0c810f30997035feabfd7b650b57a6ececbcdb5f
Vitalii Popovych <popovych@gmail.com>
2023-04-04 23:16:47 -0500

    answers for lesson 13

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+require('dotenv').config();
+const connectDB = require('./db/connect');
+const session = require('express-session');
+
 const express = require('express');
 const app = express();
 
+const taskRouter = require('./routes/tasks');
+const setMessage = require('./middleware/message');
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use('/tasks', setMessage, taskRouter);
 
 // use res.render to load up an ejs view file
 
@@ -27,5 +44,16 @@ app.get('/about', function (req, res) {
   res.render('pages/about');
 });
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+const port = 8080;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
